b74da73adda670a391bfa56e905005e0db9a1188
Потійчук Максим <161848104+fff-12@users.noreply.github.com>
2025-10-07 21:42:12 +0300

    Update deco.py

--- lab6/deco.py
+++ lab6/deco.py
@@ -3,12 +3,12 @@
 def retry_on_none(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-        if result is None:
-            while result is None:
-                a = float(input("Введіть чисельник (a): "))
-                b = float(input("Введіть знаменник (b): "))
-                resultd = func(a, b)
-                if resultd is not None:
-                    result = resultd
+        while result is None:
+            a = float(input("Введіть чисельник (a): "))
+            b = float(input("Введіть знаменник (b): "))
+            resultd = func(a, b)
+            if resultd is not None:
+                result = resultd
         return result
     return wrapper
+
